b9dc3cdb81f6e1e23570815fce9ec4dd049def1f
Anton Khorev <tony29@yandex.ru>
2015-06-22 22:53:32 +0300

    row/col swaps

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ $(function(){
 		tableNode.children('caption').append(
 			$("<button type='button' class='swap-rc'>↻</button>")
 		).append(
-			$("<button type='button' class='swap-c'>↔</button>")
+			$("<button type='button' class='swap-c'>↔</button>").click(function(){
+				tableNode.find('tr').each(function(){
+					var cell1Node=$(this).children().eq(1);
+					var cell2Node=$(this).children().eq(2);
+					cell1Node.before(cell2Node);
+				});
+			})
 		).append(
-			$("<button type='button' class='swap-r'>↕</button>")
+			$("<button type='button' class='swap-r'>↕</button>").click(function(){
+				var row1Node=tableNode.find('tr').eq(1);
+				var row2Node=tableNode.find('tr').eq(2);
+				row1Node.before(row2Node);
+			})
 		);
 	});
 });
