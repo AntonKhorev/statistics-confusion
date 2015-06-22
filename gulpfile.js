a4779be6e567390df6eb5e01a56c45abda5469f5
Anton Khorev <tony29@yandex.ru>
2015-06-22 13:46:33 +0300

    simple css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp=require('gulp');
 var plumber=require('gulp-plumber');
 var jade=require('gulp-jade');
+var sourcemaps=require('gulp-sourcemaps');
+var less=require('gulp-less');
+var autoprefixer=require('gulp-autoprefixer');
+var minifyCss=require('gulp-minify-css');
 
 var htmlSrc='src/index.jade';
+var cssSrc='src/index.less';
+
 gulp.task('html',function(){
 	gulp.src(htmlSrc)
 		.pipe(plumber())
@@ -10,8 +16,20 @@ gulp.task('html',function(){
 		.pipe(gulp.dest('public_html'));
 });
 
-gulp.task('watch',function(){
-	gulp.watch(htmlSrc,['html']);
+gulp.task('css',function(){
+	gulp.src(cssSrc)
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(autoprefixer())
+		.pipe(minifyCss({compatibility:'ie7'}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('public_html'));
 });
 
-gulp.task('default',['html']);
+gulp.task('watch',function(){
+	gulp.watch(htmlSrc,['html']);
+	gulp.watch(cssSrc,['css']);
+});
+
+gulp.task('default',['html','css']);
