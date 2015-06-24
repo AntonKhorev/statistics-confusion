975e7303e178e738c4438502f30eabfb9318b0e8
Anton Khorev <tony29@yandex.ru>
2015-06-25 02:24:45 +0300

    fractions in real formulas

--- src/util.js
+++ src/util.js
@@ -1,5 +1,19 @@
 function parseFormula(e) {
 	return e
+		.replace(/(\(.*\)|\w+)\/(\(.*\)|\w+)/,function(match,p1,p2){
+			function e(p) {
+				if (p.charAt(0)=='(') {
+					return "<span class='aux'>(</span>"+p.slice(1,-1)+"<span class='aux'>)</span>";
+				} else {
+					return p;
+				}
+			}
+			return "<span class='sfrac'>"+
+				"<span class='num'>"+e(p1)+"</span>"+
+				"<span class='aux'>/</span>"+
+				"<span class='den'>"+e(p2)+"</span>"+
+			"</span>";
+		})
 		.replace(/\bTP\b/g,"<span class='actual-true'>T</span><span class='predicted-true'>P</span>")
 		.replace(/\bFP\b/g,"<span class='actual-false'>F</span><span class='predicted-true'>P</span>")
 		.replace(/\bFN\b/g,"<span class='actual-true'>F</span><span class='predicted-false'>N</span>")
