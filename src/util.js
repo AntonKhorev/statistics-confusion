8ce39c73a0bcfd3f8382e9862f842ea1297c0c1a
Anton Khorev <tony29@yandex.ru>
2015-06-25 15:29:05 +0300

    formula terms in one span

--- src/util.js
+++ src/util.js
@@ -15,9 +15,6 @@ function parseFormula(e) {
 				"<span class='den'>"+e(p2)+"</span>"+
 			"</span>";
 		})
-		.replace(/\bTP\b/g,"<span class='actual-true'>T</span><span class='predicted-true'>P</span>")
-		.replace(/\bFP\b/g,"<span class='actual-false'>F</span><span class='predicted-true'>P</span>")
-		.replace(/\bFN\b/g,"<span class='actual-true'>F</span><span class='predicted-false'>N</span>")
-		.replace(/\bTN\b/g,"<span class='actual-false'>T</span><span class='predicted-false'>N</span>")
+		.replace(/\b(TP|FP|FN|TN)\b/g,"<span class='term $&'>$&</span>")
 	;
 }
