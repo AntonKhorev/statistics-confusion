d839f543ea9a5c86c9e7cbd0bcc6f9f33cf5f4cb
Anton Khorev <tony29@yandex.ru>
2015-07-08 19:01:42 +0300

    formula number substitution fn

--- src/util.js
+++ src/util.js
@@ -1,16 +1,102 @@
-function makeFormulaSubstitutions(e,subs) {
-	return e; // TODO
+function makeFormulaSubstitutions(formula,subs) {
+	// parse
+	function expr(formula) {
+		function stripParentheses(formula) {
+			if (formula.charAt(0)=='(') {
+				return formula.slice(1,-1);
+			} else {
+				return formula;
+			}
+		}
+		var match=formula.match(/(\(.*\)|\w+)\/(\(.*\)|\w+)/);
+		if (match) {
+			return {
+				type:'frac',
+				num:expr(stripParentheses(match[1])),
+				den:expr(stripParentheses(match[2]))
+			}
+		} else {
+			return {
+				type:'sum',
+				terms:formula.split('+')
+			}
+		}
+	}
+	function root(formula) {
+		var match=formula.match(/^(.*)=(.*)$/);
+		if (match) {
+			return {
+				type:'def',
+				lhs:match[1],
+				rhs:expr(match[2])
+			};
+		} else {
+			return expr(formula);
+		}
+	}
+	var tree=root(formula);
+
+	// substitute
+	function synth(node) {
+		function numericFraction(num,den) {
+			function gcd(a,b) {
+				if (!b) {
+					return a;
+				} else {
+					return gcd(b,a%b);
+				}
+			};
+			n=parseInt(num);
+			d=parseInt(den);
+			g=gcd(n,d);
+			return ''+parseInt(n/g)+'/'+parseInt(d/g)+'='+(n/d);
+		}
+		if (node.type=='def') {
+			return node.lhs+'='+synth(node.rhs);
+		} else if (node.type=='frac') {
+			var num=synth(node.num);
+			var den=synth(node.den);
+			if (num.indexOf('+')>=0) num='('+num+')';
+			if (den.indexOf('+')>=0) den='('+den+')';
+			if (num.match(/^\d+$/) && den.match(/^\d+$/)) {
+				return numericFraction(num,den);
+			} else {
+				return num+'/'+den;
+			}
+		} else if (node.type=='sum') {
+			var nMatches=0;
+			var sum=0;
+			node.terms.forEach(function(term){
+				if (term in subs) {
+					nMatches++;
+					sum+=subs[term];
+				}
+			});
+			if (nMatches<=1) {
+				return node.terms.map(function(term){
+					return (term in subs) ? subs[term] : term;
+				}).join('+');
+			} else if (nMatches<node.terms.length) {
+				return node.terms.filter(function(term){
+					return !(term in subs)
+				}).join('+')+'+'+sum;
+			} else {
+				return ''+sum;
+			}
+		}
+	}
+	return synth(tree);
 }
 
-function makeFormulaHtml(e) {
-	return e
+function makeFormulaHtml(formula) {
+	return formula
 		.replace(/=/g,' = ')
 		.replace(/(\(.*\)|\w+)\/(\(.*\)|\w+)/,function(match,p1,p2){
-			function e(p) {
-				if (p.charAt(0)=='(') {
-					return "<span class='aux'>(</span>"+p.slice(1,-1)+"<span class='aux'>)</span>";
+			function e(formula) {
+				if (formula.charAt(0)=='(') {
+					return "<span class='aux'>(</span>"+formula.slice(1,-1)+"<span class='aux'>)</span>";
 				} else {
-					return p;
+					return formula;
 				}
 			}
 			return "<span class='sfrac'>"+
@@ -31,6 +117,6 @@ function makeFormulaHtml(e) {
 	;
 }
 
-function makeWrappedFormulaHtml(e) {
-	return "<div class='formula' data-formula='"+e+"'>"+makeFormulaHtml(e)+"</div>";
+function makeWrappedFormulaHtml(formula) {
+	return "<div class='formula' data-formula='"+formula+"'>"+makeFormulaHtml(formula)+"</div>";
 }
---
+++ tests/util.js
@@ -0,0 +1,105 @@
+var fs=require('fs');
+eval(fs.readFileSync('../src/util.js')+'');
+var assert=require('assert');
+
+function testSubstitutions(subs,f1,f2) {
+	assert.equal(makeFormulaSubstitutions(f1,subs),f2);
+}
+
+testSubstitutions(
+	{},
+	'TERM',
+	'TERM'
+);
+
+//testSubstitutions(
+//	{TERM:null},
+//	'TERM',
+//	'TERM'
+//);
+
+testSubstitutions(
+	{TERM:23},
+	'TERM',
+	'23'
+);
+
+testSubstitutions(
+	{},
+	'TP+TN',
+	'TP+TN'
+);
+
+testSubstitutions(
+	{TP:10},
+	'TP+TN',
+	'10+TN'
+);
+
+testSubstitutions(
+	{TN:7},
+	'TP+TN',
+	'TP+7'
+);
+
+testSubstitutions(
+	{},
+	'TP+FP+FN+TN',
+	'TP+FP+FN+TN'
+);
+
+testSubstitutions(
+	{FP:3},
+	'TP+FP+FN+TN',
+	'TP+3+FN+TN'
+);
+
+testSubstitutions(
+	{FP:3,FN:4},
+	'TP+FP+FN+TN',
+	'TP+TN+7'
+);
+
+testSubstitutions(
+	{TP:10,TN:7},
+	'TP+TN',
+	'17'
+);
+
+testSubstitutions(
+	{},
+	'TP/(TP+FP)',
+	'TP/(TP+FP)'
+);
+
+testSubstitutions(
+	{TP:40},
+	'TP/(TP+FP)',
+	'40/(40+FP)'
+);
+
+testSubstitutions(
+	{TP:40,FP:10},
+	'TP/(TP+FP)',
+	'4/5=0.8'
+);
+
+testSubstitutions(
+	{},
+	'TPR=TP/(TP+FN)',
+	'TPR=TP/(TP+FN)'
+);
+
+testSubstitutions(
+	{TPR:50},
+	'TPR=TP/(TP+FN)',
+	'TPR=TP/(TP+FN)'
+);
+
+testSubstitutions(
+	{TPR:50,FN:5},
+	'TPR=TP/(TP+FN)',
+	'TPR=TP/(TP+5)'
+);
+
+console.log('tests ok');
