ef1a9a85c1300f5f98c98f568f0ba7e882ec25cb
Anton Khorev <tony29@yandex.ru>
2015-07-09 19:07:29 +0300

    some makeSum tests

--- tests/newtest.js
+++ tests/newtest.js
@@ -15,6 +15,35 @@ var assert=require('assert');
 (function(){
 	var o=makeNumber('55555555555555555555');
 	assert.equal(o.type,'float');
+	assert.equal(o.val,55555555555555555555);
+})();
+
+(function(){
+	var o=makeSum([makeSymbol('FP')]);
+	assert.deepEqual(o,{type:'sym',val:'FP'});
+})();
+
+(function(){
+	var o=makeSum([makeSymbol('FP'),makeSymbol('FN')]);
+	assert.deepEqual(o,{
+		type:'sum',
+		terms:[{type:'sym',val:'FP'},{type:'sym',val:'FN'}]
+	});
+})();
+
+(function(){
+	var o=makeSum([makeNumber('42')]);
+	assert.deepEqual(o,{type:'int',val:42});
+})();
+
+(function(){
+	var o=makeSum([makeNumber('20'),makeNum('3')]);
+	assert.deepEqual(o,{type:'int',val:23});
+})();
+
+(function(){
+	var o=makeSum([makeNumber('20'),makeNum('55555555555555555555')]);
+	assert.equal(o.type,'float');
 })();
 
 console.log('tests ok');
