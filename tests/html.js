114537c88083c540d9c5ca9ff4290066f8796910
Anton Khorev <tony29@yandex.ru>
2015-07-13 18:31:00 +0300

    no jquery for html tests

--- tests/html.js
+++ tests/html.js
@@ -1,7 +1,15 @@
 QUnit.module('convert expression to html');
 
-QUnit.test('number',function(assert){
-	var e=makeNumber('23');
-	var t=$('#qunit-fixture').html(convertExpressionToHtml(e)).text();
-	assert.equal(t,'23');
-});
+function testConvertExpressionToHtml(name,expr,text){
+	QUnit.test(name,function(assert){
+		var el=document.getElementById('qunit-fixture');
+		el.innerHTML=convertExpressionToHtml(expr);
+		assert.equal(el.textContent,text);
+	});
+}
+
+testConvertExpressionToHtml(
+	'number',
+	makeNumber('23'),
+	'23'
+);
